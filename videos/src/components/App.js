eb874869aff1f5948690cf37f8f7372bbcdb443b
Git <1219.zheng@gmail.com>
2020-01-01 15:35:00 -0800

    youtube browser app

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -183,14 +183,76 @@
 
 
 
-//Passing State as Props
+// //Passing State as Props
+// import React from 'react';
+// import SearchBar from './SearchBar';
+// import youtube from '../apis/youtube';
+// import VideoList from './VideoList';
+
+// class App extends React.Component {
+//     state = { videos: [] };
+
+//     onTermSubmit = async (term) => {
+//         const response = await youtube.get('/search', {
+//             params: {
+//                 q: term
+//             }
+//         });
+//         // console.log(response);
+//         this.setState({ videos: response.data.items });
+//     };
+
+//     render() {
+//         return (
+//             <div className="ui container">
+//                 <SearchBar onFormSubmit={this.onTermSubmit} />
+//                 <VideoList videos={this.state.videos} />
+//             </div>
+//         );
+//     }
+// }
+// export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Deeply Nested Callbacks
 import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
 
 class App extends React.Component {
-    state = { videos: [] };
+    state = { videos: [], selectedVideo: null };
 
     onTermSubmit = async (term) => {
         const response = await youtube.get('/search', {
@@ -202,11 +264,15 @@ class App extends React.Component {
         this.setState({ videos: response.data.items });
     };
 
+    onVideoSelect = (video) => {
+        console.log('From the App!', video);
+    };
+
     render() {
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit} />
-                <VideoList videos={this.state.videos} />
+                <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
             </div>
         );
     }
