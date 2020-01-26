6ce1397374fbadf870d32647864653ea0aad1182
Git <1219.zheng@gmail.com>
2020-01-25 22:27:33 -0800

    update

---
+++ videos/build/precache-manifest.7fa103a995a6c6cfe57c0b92bbde5bed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f546667e8853d80a9a0542884610ecd7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "216689da45c917670d6e",
+    "url": "/static/css/main.e9d99770.chunk.css"
+  },
+  {
+    "revision": "37d54a0ccd966f5d384b",
+    "url": "/static/js/2.acc1704d.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.acc1704d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "216689da45c917670d6e",
+    "url": "/static/js/main.66780c8b.chunk.js"
+  },
+  {
+    "revision": "7f798bcb15504130021b",
+    "url": "/static/js/runtime-main.cf9573be.js"
+  }
+]);
