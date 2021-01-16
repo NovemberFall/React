abfcbc348593edd305160d0ff33b628f437cd0d5
NovemberFall <1219.zheng@gmail.com>
2021-01-16 15:40:14 -0800

    Update unpslash.js

--- pics/src/api/unpslash.js
+++ pics/src/api/unpslash.js
@@ -4,6 +4,6 @@ export default axios.create({
     baseURL: 'https://api.unsplash.com',
 
     headers: {
-        Authorization: 'Client-ID 06162288b9848c9af42616ef69fd67b3c13761510fc01192ffa9aaabef240ffb'
+        Authorization: 'example.............................'
     }
 });
