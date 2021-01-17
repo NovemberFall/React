762d36cd5edf53140484844b68ee83b04bb895e4
NovemberFall <1219.zheng@gmail.com>
2021-01-16 17:41:33 -0800

    Update youtube.js

--- videos/src/apis/youtube.js
+++ videos/src/apis/youtube.js
@@ -1,14 +1,14 @@
-// import axios from 'axios';
-// const KEY = 'AIzaSyCKFtJ8pTpAqg2JVfyPMfUzMS-LKX9qr2E';
+import axios from 'axios';
+const KEY = 'this is an example';
 
-// export default axios.create({
-//     baseURL: 'https://www.googleapis.com/youtube/v3/',
-//     params: {
-//         part: 'snippet',
-//         maxResults: 5,
-//         key: KEY
-//     }
-// });
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3/',
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY
+    }
+});
 
 
 
